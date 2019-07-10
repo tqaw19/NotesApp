5f72e98ef22a258bee8bbf3aaedbc53aa009aeef
Alejandro Quesada <tqaw19@gmail.com>
2019-07-10 12:47:36 -0500

    first commit

---
+++ src/routes/notes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+
+const Note = require('../models/Note');
+
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', async (req, res) => {
+    const {title, description} = req.body;
+    const errors = [];
+    if(!title){
+        errors.push({text: 'Please insert a title'});
+    }
+    if(!description){
+        errors.push({text: 'Please insert a description'});
+    }
+    if(errors.length > 0){
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    }else{        
+        const newNote = new Note ({title, description});
+        await newNote.save();
+        res.redirect('/notes');
+    }
+    
+});
+
+router.get('/notes', async (req ,res) => {
+    
+    const notes = await Note.find().sort({date: 'desc'});
+    res.render('notes/all-notes', { notes });
+});
+//From FONT AWESOME PART
+
+module.exports = router;
