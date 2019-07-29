8b6e376cdae75f79ba9a3d5f23b96d40c7a359bc
Alejandro Quesada <tqaw19@gmail.com>
2019-07-28 20:39:42 -0500

    Agregar Passport

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -7,33 +7,53 @@ router.get('/notes/add', (req, res) => {
 });
 
 router.post('/notes/new-note', async (req, res) => {
-    const {title, description} = req.body;
+    const { title, description } = req.body;
     const errors = [];
-    if(!title){
-        errors.push({text: 'Please insert a title'});
+    if (!title) {
+        errors.push({ text: 'Please insert a title' });
     }
-    if(!description){
-        errors.push({text: 'Please insert a description'});
+    if (!description) {
+        errors.push({ text: 'Please insert a description' });
     }
-    if(errors.length > 0){
+    if (errors.length > 0) {
         res.render('notes/new-note', {
             errors,
             title,
             description
         });
-    }else{        
-        const newNote = new Note ({title, description});
+    } else {
+        const newNote = new Note({ title, description });
         await newNote.save();
+        req.flash('success_msg', 'Note added successfully');
+        console.log(req.body);
         res.redirect('/notes');
     }
-    
+
 });
 
-router.get('/notes', async (req ,res) => {
-    
-    const notes = await Note.find().sort({date: 'desc'});
+router.get('/notes', async (req, res) => {
+    const notes = await Note.find().sort({ date: 'desc' });
     res.render('notes/all-notes', { notes });
 });
-//From FONT AWESOME PART
+
+router.get('/notes/edit/:id', async (req, res) => {
+    const note = await Note.findById(req.params.id);
+    res.render('notes/edit-note', { note });
+});
+
+router.put('/notes/edit-note/:id', async (req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, description });
+    req.flash('success_msg', 'Note Updated Successfully');
+    res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'Note Deleted Successfully');
+    res.redirect('/notes');
+});
+
+//FROM SIGN IN - SIGN UP PART ON !!
 
 module.exports = router;
